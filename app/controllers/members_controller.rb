ed6c277653e23546a1a8ecc4041d6badec0ccae1
nozayu000 <n.yuntamu@gmail.com>
2022-01-08 00:49:47 +0000

    [add]会員削除機能

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@ class MembersController < ApplicationController
   def index
     @members = Member.order(params[:id])
   end
-  
+
   #会員情報の詳細
   def show
     @member = Member.find(params[:id])
@@ -16,19 +16,35 @@ class MembersController < ApplicationController
   def edit
     @member = Member.find(params[:id])
   end
-
+  # 会員の新規登録
   def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員登録しました"
+    else
+      render "new"
+    end
   end
 
   def update
+    @member = Member.find(params[:id])
+    @member.update(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員情報を更新しました"
+    else
+      render "edit"
+    end 
   end
 
-  def def destroy
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :member, notice: "会員を削除しました"
   end
 
   def search
     @members = Member.search(params[:q])
     render "index"
-  end 
+  end
 
 end
