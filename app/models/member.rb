4fbcacfd2211a07286f52ea06eaa4d1d9b53f9e3
nozayu000 <n.yuntamu@gmail.com>
2022-01-08 09:05:11 +0000

    [add]ymlページ

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,18 @@
 class Member < ApplicationRecord
 
   validates :nickname, presence: true,
-    format: { with: /\A[A-Za-z][A-Za-z0-9]+\z/, allow_blank: true, message: "半角英数字のみが使えます" },
+    format: { with: /\A[A-Za-z][A-Za-z0-9]+\z/, allow_blank: true, message: "は半角英数字で入力してください。" },
     length: { minimum: 2, maximum: 20,allow_blank: true},
     uniqueness: { case_sensitive: false}
 
   validates :full_name, presence: true, length: { maximum: 20}
-  validates :email, email: { allow_blank: true}
+  
+  # DBに保存する前にメールアドレスを小文字にする
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # 一意性（メールアドレスが他のユーザーと被らないように）の検証
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
 
 
   class << self
